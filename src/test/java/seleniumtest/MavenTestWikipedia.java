811954eef6892f2fc1446cbc351e761eb4f4d288
yelliaz <yiping_zhao@hotmail.com>
2020-08-11 14:29:13 -0400

    fix1

--- src/test/java/seleniumtest/MavenTestWikipedia.java
+++ src/test/java/seleniumtest/MavenTestWikipedia.java
@@ -18,19 +18,6 @@ public class MavenTestWikipedia {
   
   @Test
   public void f() {
-  		// this section is for on IE. (1)IEDriverServer.exe needs to be 32 bit (2) IE browser zoom level needs to be 100%
-		//System.setProperty("webdriver.ie.driver", "C:\\Yiping_findjob\\Tools\\Selenium_WebDriver\\IEDriverServer.exe");
-		//WebDriver driver = new InternetExplorerDriver();
-		
-		//this section is for Firefox, no extra settings needed
-		//System.setProperty("webdriver.gecko.driver", "C:\\Yiping_findjob\\Tools\\Selenium_WebDriver\\geckodriver_0.26.exe");
-		//driver = new FirefoxDriver();
-	  
-	  	//this section is for chrome, no extra settings needed
-		//System.setProperty("webdriver.chrome.driver", "C:\\Yiping_findjob\\Tools\\Selenium_WebDriver\\chromedriver.exe");
-		//String chromeResource = "./src/test/resources/drivers/chromedriver";
-		//System.setProperty("webdriver.chrome.driver", chromeResource);
-		//driver = new ChromeDriver();
 
 		System.setProperty("webdriver.gecko.driver", System.getenv("GECKOWEBDRIVER") + "//geckodriver");
 
@@ -42,17 +29,8 @@ public class MavenTestWikipedia {
         WebDriver driver = new FirefoxDriver(firefoxOptions);
 		
 		driver.get("http://www.wikipedia.org");
-		//driver.findElement(By.id("js-link-box-en")).click();
 		try {
 			Thread.sleep(5000);
-			/*
-			driver.findElement(By.xpath("//*[@class='frb-icon-btn frb-close']")).click();
-			WebElement searchBox;
-			searchBox = driver.findElement(By.id("searchInput"));
-			searchBox.sendKeys("Software Testing");
-			searchBox.submit();
-			Thread.sleep(5000);
-			*/
 		} catch (Exception e) {
 			
 		}
@@ -60,7 +38,7 @@ public class MavenTestWikipedia {
   
   @AfterTest
   public void afterTest() {
-	  driver.quit();
+	  //driver.quit();
   }
 
 }

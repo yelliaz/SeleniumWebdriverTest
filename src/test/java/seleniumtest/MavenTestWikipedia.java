0c84b1574daa738fc1114b32a38d0b6d11add7b2
yelliaz <yiping_zhao@hotmail.com>
2020-08-12 14:43:49 -0400

    enable headless options

--- src/test/java/seleniumtest/MavenTestWikipedia.java
+++ src/test/java/seleniumtest/MavenTestWikipedia.java
@@ -22,8 +22,8 @@ public class MavenTestWikipedia {
 		System.setProperty("webdriver.gecko.driver", System.getenv("GECKOWEBDRIVER") + "//geckodriver");
 
 		FirefoxBinary firefoxBinary = new FirefoxBinary();
-        //firefoxBinary.addCommandLineOptions("--headless");
-        //firefoxBinary.addCommandLineOptions("--no-sandbox");
+        firefoxBinary.addCommandLineOptions("--headless");
+        firefoxBinary.addCommandLineOptions("--no-sandbox");
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setBinary(firefoxBinary);
         driver = new FirefoxDriver(firefoxOptions);
@@ -32,13 +32,14 @@ public class MavenTestWikipedia {
 		try {
 			Thread.sleep(5000);
 		} catch (Exception e) {
-			
+		
 		}
   }
   
   @AfterTest
   public void afterTest() {
 	  driver.quit();
+	  System.out.println("The testing is finished");
   }
 
 }
